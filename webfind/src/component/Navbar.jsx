59b8c675ae5de768ff926e0bb3f3d0f955196970
Maxndrel <141615965+Maxndrel@users.noreply.github.com>
2025-03-24 16:05:47 +0100

    update nav bar

--- webfind/src/component/Navbar.jsx
+++ webfind/src/component/Navbar.jsx
@@ -4,6 +4,7 @@ import { FiLogOut, FiSettings, FiHelpCircle } from "react-icons/fi";
 import { MdWork, MdOutlineHistory, MdPeople, MdBusiness, MdOutlineTrackChanges, MdNotifications } from "react-icons/md";
 
 const Navbar = () => {
+    const [activeLink, setActiveLink] = useState("#home");
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [isNotifOpen, setIsNotifOpen] = useState(false);
@@ -37,6 +38,13 @@ const Navbar = () => {
     return `${user.firstName[0]}${user.lastName[0]}`.toUpperCase();
   };
 
+   const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#jobs", label: "Jobs" },
+    { href: "#companies", label: "Companies" },
+    { href: "#community", label: "Community" },
+  ];
+
   return (
     <nav className="fixed top-0 left-0 right-0 bg-white backdrop-blur-sm z-50 border-b border-gray-100">
       <div className="w-full container mx-auto flex items-center justify-between px-4 sm:px-6 lg:px-8 md:h-20 h-16">
@@ -44,6 +52,23 @@ const Navbar = () => {
         <div className="text-xl font-medium cursor-pointer">
           <span className="text-sky-800">Web</span>
           <span className="text-green-600">find</span>
+        </div>
+
+        
+        {/* Desktop Navigation */}
+        <div className="hidden md:flex items-center gap-10">
+          {navLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              onClick={() => setActiveLink(link.href)}
+              className={`text-sm font-medium relative after:absolute after:bottom-0 after:left-0 
+                        after:h-0.5 after:w-0 hover:after:w-full after:bg-green-600 after:transition-all 
+                        ${activeLink === link.href ? "text-green-600 after:w-full" : "text-gray-600 hover:text-gray-900"}`}
+            >
+              {link.label}
+            </a>
+          ))}
         </div>
 
         {/* Search & Icons */}
